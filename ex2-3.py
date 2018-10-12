4aaeb31d207d8f964165105ef22f76de92d2b313
zhy06 <43092699+zhy06@users.noreply.github.com>
2018-10-12 12:21:06 +0800

    Add files via upload

---
+++ ex2-3.py
@@ -0,0 +1,52 @@
+#e2.1DrawPython.py
+import turtle 
+turtle.setup(650,350,200,200)
+turtle.penup()
+turtle.fd(-250)
+turtle.pendown()
+turtle.pensize(25)
+turtle.pencolor("red")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.pencolor("orange")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+
+turtle.pencolor("yellow")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+
+turtle.pencolor("green")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+
+turtle.pencolor("cyan")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+
+turtle.pencolor("blue")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.pencolor("purple")
+turtle.seth(-40)
+for i in range(1):
+    turtle.circle(40,80)
+    turtle.circle(-40,80)
+turtle.circle(20)
+turtle.circle(40,)
+turtle.circle(80)
+turtle.fd(40)
+turtle.circle(16,180)
+turtl.fd(40*2/3)
